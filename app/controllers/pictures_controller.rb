9dbdb6bc3f608743ac951ac12c53e0f9b729cb67
jalt1 <jocelyn@ohanaparents.com>
2013-11-05 15:40:19 -0600

    View All Pictures Complete

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,4 +4,12 @@ class PicturesController < ApplicationController
     @image = @picture.source
     @caption = @picture.caption
   end
+
+  def index
+    @all_pictures = Picture.all
+    # @all_pictures.each do
+    #   @image = @picture.source
+    #   @caption = @picture.caption
+    #end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rxngif::Application.routes.draw do
   get("/picture_details/:id", { :controller => "pictures", :action => "show" })
+  get("/all_pictures", { :controller => "pictures", :action => "index" })
 end
